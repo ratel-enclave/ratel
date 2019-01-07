c58725957214f0def6bf15f8f130b5af0dc8f1d3
Pinghai Yuan <pinghaiyuan@gmail.com>
2019-01-07 11:33:06 +0800

    Set the program areana to 0x80000000 bytes 	modified:   Enclave/dynamorio.config.xml 	modified:   Enclave/sgx_edge/sgx_mm.c

--- Enclave/sgx_edge/sgx_mm.c
+++ Enclave/sgx_edge/sgx_mm.c
@@ -87,7 +87,8 @@ extern void* g_enclave_image_base;
 #define SGX_PAGE_SIZE 4096
 
 // Ensure the external prog-arena is only with 0x7fffe0000000 ~ 0x7ffff8000000
-#define EXTN_MEM_REGION     (byte*)0x7fffe0000000
+// Ensure the external prog-arena is only with 0x7fff80000000 ~ 0x7fffffffffff  //2G
+#define EXTN_MEM_REGION     (byte*)0x7fff80000000
 
 
 /* exported out for debugging, we have unit-tests */
