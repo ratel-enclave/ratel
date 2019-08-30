cb854a6bbc8c45f2be74f3626edcd8eba73edf58
Pinghai Yuan <pinghaiyuan@gmail.com>
2019-08-30 14:57:28 +0800

    Remove extra code  Changes to be committed: 	modified:   signal_SIGALARM.c

--- dbi_test/signal_SIGALARM.c
+++ dbi_test/signal_SIGALARM.c
@@ -21,21 +21,13 @@ int main(int argc, char *argv[])
     sVal.sa_sigaction = HandleSignal;
 
     myPID = getpid();
-    myG_PID = getpgid(myPID);
-    printf("\nMy process id = %d.\n", myPID);
-    printf("My process group id = %d.\n", myG_PID);
+    printf("\nPID = %d\n", myPID);
 
     // Register for SIGINT
     sigaction(SIGALRM, &sVal, NULL);
 
-    myPID = getpid();
-    myG_PID = getpgid(myPID);
-    printf("\nParent: My process id = %d.\n", myPID);
-    printf("Parent: My process group id = %d.\n", myG_PID);
-
     locked = 1;
     alarm(2);
-
     while (locked)
     {
     }
@@ -50,12 +42,8 @@ void HandleSignal(int sig, siginfo_t *si, void *context)
     {
 
         case SIGALRM:
-            printf("Get a sigalarm\n");
+            printf("Get a SIGALRM\n");
             locked = 0;
-            if (si->si_code == CLD_EXITED || si->si_code == CLD_KILLED)
-            {
-                printf("   Process %d is done!\n", si->si_pid);
-            }
             break;
     }
 }
