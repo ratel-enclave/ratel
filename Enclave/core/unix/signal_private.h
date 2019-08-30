57bd3a55799084c49be36a95d569f070b19e448c
Pinghai Yuan <pinghaiyuan@gmail.com>
2019-08-30 14:30:09 +0800

    Update signal processing chain  Changes to be committed: 	modified:   App/syscall.c 	modified:   Enclave/core/unix/signal.c 	modified:   Enclave/core/unix/signal_private.h 	modified:   Enclave/sgx_edge/call_out.c

--- Enclave/core/unix/signal_private.h
+++ Enclave/core/unix/signal_private.h
@@ -187,6 +187,74 @@ typedef struct _sigcxt_pkg_t {
     siginfo_t       info;
 }sigcxt_pkg_t;
 
+/* Begin: Added by Pinghai */
+typedef enum _sgx_exception_vector_t
+{
+    SGX_EXCEPTION_VECTOR_DE = 0,  /* DIV and DIV instructions */
+    SGX_EXCEPTION_VECTOR_DB = 1,  /* For Intel use only */
+    SGX_EXCEPTION_VECTOR_BP = 3,  /* INT 3 instruction */
+    SGX_EXCEPTION_VECTOR_BR = 5,  /* BOUND instruction */
+    SGX_EXCEPTION_VECTOR_UD = 6,  /* UD2 instruction or reserved opcode */
+    SGX_EXCEPTION_VECTOR_MF = 16, /* x87 FPU floating-point or WAIT/FWAIT instruction */
+    SGX_EXCEPTION_VECTOR_AC = 17, /* Any data reference in memory */
+    SGX_EXCEPTION_VECTOR_XM = 19, /* SSE/SSE2/SSE3 floating-point instruction */
+} sgx_exception_vector_t;
+
+typedef enum _sgx_exception_type_t
+{
+    SGX_EXCEPTION_HARDWARE = 3,
+    SGX_EXCEPTION_SOFTWARE = 6,
+} sgx_exception_type_t;
+
+#if defined (_M_X64) || defined (__x86_64__)
+typedef struct _cpu_context_t
+{
+    uint64_t rax;
+    uint64_t rcx;
+    uint64_t rdx;
+    uint64_t rbx;
+    uint64_t rsp;
+    uint64_t rbp;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+    uint64_t rflags;
+    uint64_t rip;
+} sgx_cpu_context_t;
+#else
+typedef struct _cpu_context_t
+{
+    uint32_t eax;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t ebx;
+    uint32_t esp;
+    uint32_t ebp;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t eflags;
+    uint32_t eip;
+} sgx_cpu_context_t;
+#endif
+
+typedef struct _exception_info_t
+{
+    sgx_cpu_context_t      cpu_context;
+    sgx_exception_vector_t exception_vector;
+    sgx_exception_type_t   exception_type;
+	/* Begin: Added by Pinghai */
+    void*                  sigcxt_pkg;  /* pointing to external structure that keeps all informaiton */
+	/* End: Added by Pinghai */
+} sgx_exception_info_t;
+/* End: Added by Pinghai */
+
 /* SIGCXT_FROM_UCXT is in os_public.h */
 # define SIGMASK_FROM_UCXT(ucxt) (&((ucxt)->uc_sigmask))
 
